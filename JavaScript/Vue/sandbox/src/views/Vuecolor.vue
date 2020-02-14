49e2cdf30e65db0d47e422be3498005a542d380c
kyoruni <kyoruni@gmail.com>
2020-02-14 15:11:46 +0900

    :wrench: #10 カラーピッカー表示
    表示しただけ

--- JavaScript/Vue/sandbox/src/views/Vuecolor.vue
+++ JavaScript/Vue/sandbox/src/views/Vuecolor.vue
@@ -1,5 +1,61 @@
 <template>
   <div class="vue-color">
-    vue color
+    <div class="d-flex">
+      <div class="section1 mt-2">
+        section1
+      </div>
+      <div class="section2 mt-2">
+        section2
+      </div>
+      <div class="section3 mt-2">
+        section3
+      </div>
+    </div>
+    <div class="mt-2">
+      <photoshop-picker :value="colors"></photoshop-picker>
+    </div>
   </div>
 </template>
+
+<script>
+import { Photoshop } from 'vue-color'
+
+export default {
+  components: {
+    'photoshop-picker': Photoshop
+  },
+  data () {
+    return {
+      colors: {
+        hex: '#ff99cc',
+        hsl: { h: 150, s: 0.5, l: 0.2, a: 1 },
+        hsv: { h: 150, s: 0.66, v: 0.30, a: 1 },
+        rgba: { r: 25, g: 77, b: 51, a: 1 },
+        a: 1
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.section1 {
+  height: 100px;
+  width: 100px;
+  color: white;
+  background: red;
+}
+
+.section2 {
+  height: 100px;
+  width: 100px;
+  color: white;
+  background: blue;
+}
+
+.section3 {
+  height: 100px;
+  width: 100px;
+  color: white;
+  background: green;
+}
+</style>
