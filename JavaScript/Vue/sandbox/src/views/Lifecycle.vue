e979c43a370c01ccbe9a496a4fd0264937fad80a
kyoruni <kyoruni@gmail.com>
2020-02-14 14:43:17 +0900

    :wrench: #2 created〜destroyまでお試し

--- JavaScript/Vue/sandbox/src/views/Lifecycle.vue
+++ JavaScript/Vue/sandbox/src/views/Lifecycle.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="lifecycle">
     ライフサイクル
+    <div>
+      <button class="btn btn-danger" @click="destroyButton">インスタンス破棄！</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      item: 'aaaaa'
+    }
+  },
+  created () {
+    console.log('created >>>>')
+    console.log('インスタンスが生成されて、データの準備ができた')
+    console.log(this.item)
+  },
+  mounted () {
+    console.log('mounted >>>>')
+    console.log('インスタンスがページに描画された')
+    console.log(this.item)
+  },
+  beforeDestroy () {
+    console.log('beforeDestroy >>>>')
+    console.log('インスタンス破棄する直前')
+    console.log(this.item)
+  },
+  destroyed () {
+    console.log('destroyed >>>>')
+    console.log('インスタンス破棄した')
+    console.log(this.item)
+  },
+  methods: {
+    destroyButton () {
+      this.$destroy()
+    }
+  }
+}
+</script>
