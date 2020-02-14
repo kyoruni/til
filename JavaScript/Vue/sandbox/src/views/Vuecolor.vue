600e10ead8846bbfc2d993545d6310314b944a48
kyoruni <kyoruni@gmail.com>
2020-02-14 16:41:01 +0900

    :wrench: #10 初期値はピンクにする

--- JavaScript/Vue/sandbox/src/views/Vuecolor.vue
+++ JavaScript/Vue/sandbox/src/views/Vuecolor.vue
@@ -28,9 +28,6 @@ export default {
     return {
       colors: {
         hex: '#ff99cc',
-        hsl: { h: 150, s: 0.5, l: 0.2, a: 1 },
-        hsv: { h: 150, s: 0.66, v: 0.30, a: 1 },
-        rgba: { r: 25, g: 77, b: 51, a: 1 },
         a: 1
       },
       presetColors: [
